27fc63c033bcc0d088d5ef8f6baace95fdca03ce
duyvo <duyvo3001@gmail.com>
2023-06-15 15:48:50 +0700

    paid create sussces and don't update qty of product

--- src/controllers/PaidProductController.js
+++ src/controllers/PaidProductController.js
@@ -2,31 +2,28 @@ import connec from '../configs/connectDBmongo.js'
 import modelHoaDon from '../models/HoaDon.model.js';
 
 const paidProduct = async (req, res) => {
+    // update qty of product
     let { searchCustomer, Discount } = req.body.formData;
     let Product = []
+
     await req.body.Render?.map((key) => {
         Product.push({ NameProduct: key.NameProduct, Qty: key.Qty })
     })
-    console.log(await createIDPaid())
-    // can chuyen du lieu vao data S
-    // lap Id Hoadon 
-    // gui du lieu vao mongodb
+
+    const IDPaidOrder = await createIDPaid()
 
     let data = {
-        searchCustomer, Discount, Product
+        IDPaidOrder, searchCustomer, Discount, Product, Date: Date.now()
     }
 
-    const checkData = await connec.getDB().collection("HoaDon").find({}).toArray();
-
-    console.info(checkData)
-    // }
     const result = await connec.getDB().collection("HoaDon").insertOne(data)
-    // await modelHoaDon.HoaDonmodel(data)
+    console.info(result)
     await res.status(200).json({ message: 'oke' });
 }
 
+
 const createIDPaid = async () => {
-    return await connec.getDB().collection("HoaDon").find().toArray();
+    return Math.floor(Math.random() * Math.floor(Math.random() * Date.now())).toString(16)
 }
 
 export default { paidProduct }
