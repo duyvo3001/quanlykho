44e8e3f7dd637c4b0d654a5cdc090ee32e3069b3
duyvo <duyvo3001@gmail.com>
2023-08-24 16:17:22 +0700

    update dashboard

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,8 +1,10 @@
 import connec from '../configs/connectDBmongo.js'
+import data from "../services/renderdataHang";
 
 let DashBoard = async (req, res) => {
     let QuantityInventory = 0
     let QuantityExport = 0
+
     let Inventory = await connec.getDB().collection("Hang").find({
         NgayNhap: {
             $gte: new Date("2023" + "-" + "01" + "-01"),
@@ -16,12 +18,29 @@ let DashBoard = async (req, res) => {
             $lt: new Date("2023" + "-" + "12" + "-31")
         }
     }).toArray();
-    Export
-    .map((key) => {
-        for (let i = 0; i < key.Product.length; i++) {
-            QuantityExport += Number(key.Product[i]?.Qty)
-        }
+
+    let ProductObj = []
+    const limit = 16;
+    const skip = 0;
+    let Product = await data.result('Hang', 'renderData', '', limit, skip)
+
+    Product.map((key, index) => {
+        ProductObj.push({
+            id: index + 1,
+            MaLK: key.MaLK,
+            TenLK: key.TenLK,
+            Soluong: key.Soluong,
+            NgayNhap : key.NgayNhap,
+            TinhTrangHang: key.TinhTrangHang
+        });
     })
+
+    Export
+        .map((key) => {
+            for (let i = 0; i < key.Product.length; i++) {
+                QuantityExport += Number(key.Product[i]?.Qty)
+            }
+        })
 
     Inventory
         .filter((key) => {
@@ -33,14 +52,14 @@ let DashBoard = async (req, res) => {
         .map((key) => {
             return key
         })
-    console.info(QuantityExport)
+
     let result = {
         Inventory: Inventory.length,
         InventoryQuantity: QuantityInventory,
         Export: Export.length,
         ExportQuantity: QuantityExport
     }
-    res.status(200).json({ result })
+    res.status(200).json({ result, ProductObj })
 }
 
 export default { DashBoard }
