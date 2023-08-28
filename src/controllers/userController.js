4444efea1d78184723c20b46f23f03b34358344e
duyvo3001 <74234968+duyvo3001@users.noreply.github.com>
2023-08-29 00:13:02 +0700

    check

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -53,9 +53,8 @@ let createUser = async (req, res) => {
 
       return res.status(201).json({ message: 'created successfully' });
     }
-    else console.log("test failed ");
-
-    return res.status(400).json({ message: 'failed' });
+    else
+      return res.status(400).json({ message: 'failed' });
   }
 }
 
@@ -104,6 +103,7 @@ let updateUser = async (req, res) => {
   let {
     MaNV, TenNV, NgaySinh, GioiTinh, USER_NV, pass_nv, repass_nv, SDT, Email, DiaChi, _id
   } = req.body.formData;
+
   let arrData = [MaNV, TenNV, NgaySinh, GioiTinh, USER_NV, pass_nv, repass_nv, SDT, Email, DiaChi];
 
   //check special characters
