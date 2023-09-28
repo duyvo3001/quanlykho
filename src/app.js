d6ed972510956dea3e49d7c5fae71272053d7ab2
duyvo3001 <74234968+duyvo3001@users.noreply.github.com>
2023-09-28 10:10:59 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to w
-  // res.setHeader('Access-Control-Allow-Origin', process.env.UrlWebClient);
+  res.setHeader('Access-Control-Allow-Origin', "*");
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
